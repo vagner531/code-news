2cef1f9ebcc1559d25696ee7e56061d50f926cce
vagner santos <vagner131417@gmail.com>
2023-04-15 14:03:23 -0300

    feat: add iterating in jsx post properties

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/vagner531.png',
+      name: 'Vagner',
+      role: 'Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},
+      { type: 'link', content: 'jane.design/doctorcare', },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/VanshajPoonia.png',
+      name: 'Vanshaj Poonia',
+      role: 'CEO @hypnosysindia',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},
+      { type: 'link', content: 'jane.design/doctorcare', },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00')
+  },
+]
+
 export function App() {
   return (
     <div>
@@ -15,8 +46,15 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return(
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
