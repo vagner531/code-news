46cf3ef31d999c27041580ba7138fff5f2faba10
vagner santos <vagner131417@gmail.com>
2023-04-17 15:36:44 -0300

    feat: update function like

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,16 +2,23 @@ import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "./Avatar";
 
 import styles from "./Comment.module.css";
+import { useState } from "react";
 
 export function Comment({ content, onDeleteComment }) {
-  function handleDeleteComment() {
+  const [likeCount, setLikeCount] = useState(0);
 
-    onDeleteComment(content)
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
   }
 
   return (
     <div className={styles.comment}>
-      
       <Avatar hasBorder={false} src="https://github.com/vagner531.png" />
 
       <div className={styles.commentBox}>
@@ -24,15 +31,16 @@ export function Comment({ content, onDeleteComment }) {
               </time>
             </div>
             <button onClick={handleDeleteComment} title="Deletar comentário">
-              <Trash size={23}/>
+              <Trash size={23} />
             </button>
           </header>
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir
+            <span>{likeCount}</span>
           </button>
         </footer>
       </div>
